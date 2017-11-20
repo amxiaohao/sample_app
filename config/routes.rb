455c17713ab0f819409fabe9e5e829e9601d46a5
amxiaohao <sudoxiaohao@gmail.com>
2017-11-20 13:45:19 +0800

    完成网页基本静态内容

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  #把发给 /static_pages/home 的请求映射到 StaticPages 控制器的 home 动作上
+  root 'static_pages#home'
+  get  'static_pages/home'
+  get  'static_pages/help'
+  get  'static_pages/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
