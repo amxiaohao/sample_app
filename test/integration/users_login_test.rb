d11ac2baf6405db10b5d5b53add2d1d4ab6f53f6
zhangxiaohao <sudoxiaohao@gmail.com>
2017-11-23 21:30:33 +0800

    some fix

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -7,13 +7,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   end
 
   test "login with valid information" do
+    #登录模拟
     post login_path, params: { session: { email:    @user.email, 
                                           password: "password" } }
+    #登录测试
     assert_redirected_to user_url(@user)
     follow_redirect!
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+    
+    #登出测试
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
