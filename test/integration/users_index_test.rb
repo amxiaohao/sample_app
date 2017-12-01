026a1c446b2ddc48b62cc28f94df9910e15eb678
amxiaohao <sudoxiaohao@gmail.com>
2017-12-01 11:23:55 +0800

    用户激活功能

---
+++ test/integration/users_index_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class UsersIndexTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:foo)
+  end
+
+  #测试主页有分页选项
+  test "index including pagination" do
+    post login_path, params: { session: { email:    @user.email, 
+                                          password: "password" } }
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    
+    User.paginate(page: 1).each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+    end
+  end
+end
